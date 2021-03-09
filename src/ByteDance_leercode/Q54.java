ca71689775471b09286e025df072348f0a18d9b8
feiqiu <448407561@qq.com>
2021-03-09 21:10:13 +0800

    1

---
+++ src/ByteDance_leercode/Q54.java
@@ -0,0 +1,44 @@
+package ByteDance_leercode;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * @author 冯若航
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/3/9 20:43
+ */
+public class Q54 {
+    public List<Integer> spiralOrder(int[][] matrix){
+        int left=0,right=matrix[0].length-1,top=0,below=matrix.length-1;//得到上下左右边界
+        List<Integer> list =new ArrayList<>();
+        int sum=(right+1)*(below+1);//元素总个数
+        int num=1;
+
+        //开始遍历
+        while(num<=sum){
+            for(int i=left;i<=right&&num<=sum;i++){//从左到右
+                list.add(matrix[top][i]);
+                num++;
+            }
+            top++;
+            for(int i=top;i<=below&&num<=sum;i++){//从上到下
+                list.add(matrix[i][right]);
+                num++;
+            }
+            right--;
+            for(int i=right;i>=left&&num<=sum;i--){//从右到左
+                list.add(matrix[below][i]);
+                num++;
+            }
+            below--;
+            for(int i=below;i>=top&&num<=sum;i--){//从下往上
+                list.add(matrix[i][left]);
+                num++;
+            }
+            left++;
+        }
+        return list;
+
+
+    }
+}
